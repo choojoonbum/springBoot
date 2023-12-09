db372b501f8a9a994c7f8b05c498b22abf426d3f
chooJoonbum <cjb3333@naver.com>
2023-12-09 16:28:17 +0900

    다대다 연관설정

---
+++ src/main/java/org/choo/springboot/entity/Member2.java
@@ -0,0 +1,20 @@
+package org.choo.springboot.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@Table(name = "m_member")
+public class Member2 extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mid;
+    private String pw;
+    private String nickname;
+}
