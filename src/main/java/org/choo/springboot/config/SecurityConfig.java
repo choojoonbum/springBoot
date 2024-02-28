2800cd9d383a6c32908a4f857b69cf36204dde87
chooJoonbum <cjb3333@naver.com>
2024-02-28 17:05:26 +0900

    api서버 필터 적용 및 인증 처리

--- src/main/java/org/choo/springboot/config/SecurityConfig.java
+++ src/main/java/org/choo/springboot/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package org.choo.springboot.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.choo.springboot.security.filter.ApiCheckFilter;
+import org.choo.springboot.security.filter.ApiLoginFilter;
 import org.choo.springboot.security.handler.ClubLoginSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -14,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @Log4j2
@@ -21,7 +26,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Autowired
-    private UserDetailsService userDetailService;
+    private UserDetailsService userDetailsService;
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -43,18 +48,43 @@ public class SecurityConfig {
         //    auth.antMatchers("/sample/all").permitAll();
         //    auth.antMatchers("/sample/member").hasRole("USER");
         //});
+
+        //AuthenticationManager설정
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        // Get AuthenticationManager
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+        //반드시 필요
+        http.authenticationManager(authenticationManager);
+
         http.formLogin();
         http.csrf().disable();
         http.logout();
-
         http.oauth2Login().successHandler(successHandler());
-        http.rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailService);
+        http.rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailsService);
+        http.addFilterBefore(apiCheckFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(apiLoginFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
+    public ApiLoginFilter apiLoginFilter(AuthenticationManager authenticationManager) throws Exception{
+
+        ApiLoginFilter apiLoginFilter =  new ApiLoginFilter("/api/login");
+        apiLoginFilter.setAuthenticationManager(authenticationManager);
+
+        return apiLoginFilter;
+    }
+
+
     @Bean
     public ClubLoginSuccessHandler successHandler() {
         return new ClubLoginSuccessHandler(passwordEncoder());
+    }
+
+    @Bean
+    public ApiCheckFilter apiCheckFilter() {
+        return new ApiCheckFilter("/notes/**/*");
     }
 }
